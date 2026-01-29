46d1dfb4b47f74d9e627becdbe50f814e2d4e334
azure <feelinglonely905@gmail.com>
2026-01-30 01:13:16 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,51 +1,33 @@
-const comicList = document.getElementById("comic-list");
-const updateList = document.getElementById("update-list");
-const newList = document.getElementById("new-list");
+const list = document.getElementById("comic-list");
 const search = document.getElementById("search");
 
-function card(c) {
-  return `
-  <div class="card" onclick="openComic('${c.id}')">
-    <img src="${c.cover}">
-    <h3>${c.title}</h3>
-    <small>${c.genre} • ${c.chapters.length} Ch</small>
-  </div>
-  `;
+function render(data) {
+  list.innerHTML = "";
+  data.forEach((c, i) => {
+    list.innerHTML += `
+      <div class="card" onclick="openComic(${i})">
+        <img src="${c.cover}">
+        <h3>${c.title}</h3>
+        <small>${c.genre} • ${c.chapters.length} Ch</small>
+      </div>
+    `;
+  });
 }
 
-function openComic(id) {
-  location.href = `reader.html?comic=${id}&chapter=1`;
+function openComic(i) {
+  // buka chapter TERBARU
+  const chapters = comics[i].chapters;
+  const last = chapters[chapters.length - 1];
+  window.open(last.drive, "_blank");
 }
 
-/* SEMUA KOMIK */
-function renderAll(data) {
-  comicList.innerHTML = data.map(card).join("");
-}
-
-/* NEW MANHWA (URUT DARI PALING BARU) */
-function renderNew() {
-  const sorted = [...comics].sort((a,b)=> 
-    new Date(b.update) - new Date(a.update)
-  );
-  newList.innerHTML = sorted.slice(0,6).map(card).join("");
-}
-
-/* NEW CHAPTER UPDATE */
-function renderUpdate() {
-  const sorted = [...comics].sort((a,b)=> 
-    new Date(b.update) - new Date(a.update)
-  );
-  updateList.innerHTML = sorted.slice(0,6).map(card).join("");
-}
-
-renderAll(comics);
-renderNew();
-renderUpdate();
+render(comics);
 
 /* SEARCH */
 search.addEventListener("input", () => {
   const v = search.value.toLowerCase();
-  renderAll(
+  render(
     comics.filter(c => c.title.toLowerCase().includes(v))
   );
 });
+       
