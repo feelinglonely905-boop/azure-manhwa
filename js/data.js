a4a580e4ce9255d5ac14c6f947ad2a286836db0d
azure <feelinglonely905@gmail.com>
2026-01-30 00:18:26 +0700

    Add chapters and update genre for 'How to Send Your Husband to Hell'
    Added chapters with images to the comic 'How to Send Your Husband to Hell' and updated its genre.

--- js/data.js
+++ js/data.js
@@ -1,22 +1,32 @@
 const comics = [
   {
+    id: "How-to-Send-Your-Husband-to-Hell",
     title: "How to Send Your Husband to Hell",
-    genre: "Romance",
+    genre: "Romance,Historical",
     cover: "https://drive.google.com/uc?export=view&id=1Glujra3Rs_6HPbK7AeGAuUnu3DDjYBwF",
-  },
-  {
-    title: "Mythic Summoner",
-    genre: "Fantasy",
-    cover: "https://via.placeholder.com/300x450?text=Manhwa",
-  },
-  {
-    title: "Love Dungeon",
-    genre: "Romance",
-    cover: "https://via.placeholder.com/300x450?text=Manhwa",
-  },
-  {
-    title: "Isekai System",
-    genre: "Isekai",
-    cover: "https://via.placeholder.com/300x450?text=Manhwa",
+
+    chapters: [
+      {
+        chapter: 1,
+        images: [
+          "https://drive.googleusercontent.com/uc?id=1Tj1fXqHKpUJGaUDoZIuABsRlgPi7-n1y",
+          "https://drive.googleusercontent.com/uc?id=1CRZhoP-aTr9J2B76L1DQNhP60UlZwUy7",
+          "https://drive.googleusercontent.com/uc?id=1lMnkw8zxl1T_9HAiQhIU016CYa-CSHQ6",
+          "https://drive.googleusercontent.com/uc?id=1eECvkpqsIzZ_XeFjsPetsIjk25eB_iKA"
+        ]
+      },
+      {
+        chapter: 2,
+        images: [
+          "https://drive.googleusercontent.com/uc?id=1fEv0eulgDu2RNKzJQyPw4rxlBbszcJ8M",
+          "https://drive.googleusercontent.com/uc?id=1iZU0oiA_0wc-PKctZ_fGQIM5wQsTqlUV",
+          "https://drive.googleusercontent.com/uc?id=1pM8M-hlbXNO86LRNRtTU4U7SUldP2uHz",
+          "https://drive.googleusercontent.com/uc?id=13HmqfZy7U71w1wZgmtHdR2M-7gWSyaOP",
+          "https://drive.googleusercontent.com/uc?id=1G4GoOIJIkhA_FGXXCZWsomjSMSJfg-t2",
+          "https://drive.googleusercontent.com/uc?id=1NHlKw5FWDe4xxpYo2sJjcIemSzUS4lyu",
+          "https://drive.googleusercontent.com/uc?id=1bl8MgnXLwtORgPEzP0jHaZVIyNVqBw0U"
+        ]
+      }
+    ]
   }
 ];
