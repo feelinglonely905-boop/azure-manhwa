fcd418cff68c459664eababc8760d2f492af5596
azure <feelinglonely905@gmail.com>
2026-01-30 01:12:01 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,32 +1,29 @@
 const comics = [
   {
-    id: "How-to-Send-Your-Husband-to-Hell",
     title: "How to Send Your Husband to Hell",
     genre: "Romance,Historical",
     cover: "https://drive.google.com/uc?export=view&id=1Glujra3Rs_6HPbK7AeGAuUnu3DDjYBwF",
-
     chapters: [
       {
         chapter: 1,
-        images: [
-          "https://drive.googleusercontent.com/uc?id=1Tj1fXqHKpUJGaUDoZIuABsRlgPi7-n1y",
-          "https://drive.googleusercontent.com/uc?id=1CRZhoP-aTr9J2B76L1DQNhP60UlZwUy7",
-          "https://drive.googleusercontent.com/uc?id=1lMnkw8zxl1T_9HAiQhIU016CYa-CSHQ6",
-          "https://drive.googleusercontent.com/uc?id=1eECvkpqsIzZ_XeFjsPetsIjk25eB_iKA"
-        ]
+        drive: "https://drive.google.com/drive/folders/16c4EVi7ykqvWnunmS9lfHlLaxWLfEihW"
       },
       {
         chapter: 2,
-        images: [
-          "https://drive.googleusercontent.com/uc?id=1fEv0eulgDu2RNKzJQyPw4rxlBbszcJ8M",
-          "https://drive.googleusercontent.com/uc?id=1iZU0oiA_0wc-PKctZ_fGQIM5wQsTqlUV",
-          "https://drive.googleusercontent.com/uc?id=1pM8M-hlbXNO86LRNRtTU4U7SUldP2uHz",
-          "https://drive.googleusercontent.com/uc?id=13HmqfZy7U71w1wZgmtHdR2M-7gWSyaOP",
-          "https://drive.googleusercontent.com/uc?id=1G4GoOIJIkhA_FGXXCZWsomjSMSJfg-t2",
-          "https://drive.googleusercontent.com/uc?id=1NHlKw5FWDe4xxpYo2sJjcIemSzUS4lyu",
-          "https://drive.googleusercontent.com/uc?id=1bl8MgnXLwtORgPEzP0jHaZVIyNVqBw0U"
-        ]
+        drive: "https://drive.google.com/drive/folders/1ma_VpfHBADd9zN4w72GivaiXvs1F0ITV"
+      }
+    ]
+  },
+  {
+    title: "This Wasn't in My Adoption Plan",
+    genre: "Reinkarnasi,Historical",
+    cover: "https://drive.google.com/uc?export=view&id=1kSIlNN_SCjdStyZEif0Ofb0y4OwPnpS6",
+    chapters: [
+      {
+        chapter: 48,
+        drive: "https://drive.google.com/drive/folders/1kI6KpumjyIcgrB9XOj2NUfamBQ9j1I36"
       }
     ]
   }
 ];
+    
